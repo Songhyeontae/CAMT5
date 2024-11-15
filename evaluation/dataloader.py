8fd9838f8535d497c7abad05a2afd37e1479ca31
Song Hyeontae (Harry.song) <hyeontae@kaist.ac.kr>
2024-11-16 02:16:47 +0900

    chore: lint (#11)

--- evaluation/dataloader.py
+++ evaluation/dataloader.py
@@ -1,58 +1,57 @@
-from model.representation import SMILES
-from typing import Tuple, List, Iterator
-
-from transformers import PreTrainedTokenizer
-from model.loader import ModelLoader, Model
-from model.representation import Representation
-
-from utils import to_absolute_path
 import dataclasses
+from typing import Iterator, List, Tuple
+
 import pandas as pd
 import torch
-from torch.utils.data import Dataset, DataLoader
+from torch.utils.data import DataLoader, Dataset
+from transformers import PreTrainedTokenizer
+
 from evaluation.config import DataConfig
+from model.loader import Model, ModelLoader
+from model.representation import SMILES, Representation
+from utils import to_absolute_path
 
 DESCRIPTION_TEMPLATE = (
     "Definition: You are given a molecule description in English. "
     "Your job is to generate the molecule fragments that fit the description. "
     "Now complete the following example - "
-    "Input: {description} Output: "
-)
+    "Input: {description} Output: ")
+
 
 class EvalDataset(Dataset):
+
     def __init__(self, file_path: str, chunk_size: int = 1024):
         assert file_path.endswith(".csv"), "Only CSV files are supported"
         self.file_path = file_path
         self.chunk_size = chunk_size
-        self.data_length = sum(1 for _ in open(file_path)) - 1 # Skip the header
-    
+        self.data_length = sum(1
+                               for _ in open(file_path)) - 1  # Skip the header
+
     def __len__(self):
         return self.data_length
-    
+
     def __getitem__(self, idx: int) -> Tuple[str, str]:
         chunk_idx = idx // self.chunk_size
         row_idx = idx % self.chunk_size
-        
-        chunk = pd.read_csv(self.file_path, 
+
+        chunk = pd.read_csv(self.file_path,
                             sep="\t",
-                            skiprows=chunk_idx * self.chunk_size + 1, 
-                            nrows=self.chunk_size, header=None)
-        
+                            skiprows=chunk_idx * self.chunk_size + 1,
+                            nrows=self.chunk_size,
+                            header=None)
+
         row = chunk.iloc[row_idx]
         target, description = row[1], row[2]
         full_description = DESCRIPTION_TEMPLATE.format(description=description)
 
         return target, full_description
 
+
 def get_dataloader(data_config: DataConfig) -> DataLoader:
-    dataset = EvalDataset(
-        file_path=data_config.data_path,
-        chunk_size=data_config.chunk_size
-    )
-    return DataLoader(
-        dataset,
-        batch_size=data_config.batch_size,
-        num_workers=data_config.num_workers,
-        prefetch_factor=data_config.prefetch_factor,
-        shuffle=False
-        )
+    dataset = EvalDataset(file_path=data_config.data_path,
+                          chunk_size=data_config.chunk_size)
+    return DataLoader(dataset,
+                      batch_size=data_config.batch_size,
+                      num_workers=data_config.num_workers,
+                      prefetch_factor=data_config.prefetch_factor,
+                      shuffle=False)
--- evaluation/task/run.py
+++ evaluation/task/run.py
@@ -1,37 +1,36 @@
-import logging
 import dataclasses
+import logging
 import os
-import csv
-import torch
-import torch.nn.functional as F
-from torch.utils.data import DataLoader
-from typing import List, Tuple, Dict, Union
-from itertools import islice
 from collections import defaultdict
-from itertools import islice
+from typing import Dict, List, Tuple, Union
 
-from core.task import BaseTaskCls
-from evaluation.config import EvalConfig, PredictConfig, Device, Confidence
-from model.config import ModelConfig
-from model.loader import ModelLoader, Model
-from model.representation import Representation
-from metrics.text2mol_metrics import get_text2mol_metrics
-
+import torch
+from tqdm import tqdm
 from transformers import PreTrainedTokenizer
 from transformers.generation.utils import GenerateOutput
-from evaluation.dataloader import get_dataloader
-from evaluation.confidence import get_perplexity, get_entropy, get_len_norm_entropy, get_importance_weighted_entropy, get_rdk, get_exact
 
-from tqdm import tqdm
+from core.task import BaseTaskCls
+from evaluation.confidence import (get_entropy, get_exact,
+                                   get_importance_weighted_entropy,
+                                   get_len_norm_entropy, get_perplexity,
+                                   get_rdk)
+from evaluation.config import Confidence, Device, EvalConfig, PredictConfig
+from evaluation.dataloader import get_dataloader
+from metrics.text2mol_metrics import get_text2mol_metrics
+from model.config import ModelConfig
+from model.loader import Model, ModelLoader
+from model.representation import Representation
 
 logger = logging.getLogger(__name__)
+
 
 @dataclasses.dataclass
 class MolLM:
     model: Model
     tokenizer: PreTrainedTokenizer
     representation: Representation
-    
+
+
 class App(BaseTaskCls):
 
     def __init__(
@@ -50,20 +49,15 @@ class App(BaseTaskCls):
         mol_lms = {}
         for model_config in self.model_configs:
             model_loader = ModelLoader(model_config)
-            mol_lm = MolLM(
-                model=model_loader.load_model(),
-                tokenizer=model_loader.load_tokenizer(),
-                representation=model_loader.load_representation()
-            )
+            mol_lm = MolLM(model=model_loader.load_model(),
+                           tokenizer=model_loader.load_tokenizer(),
+                           representation=model_loader.load_representation())
             mol_lms[model_config.representation_type] = mol_lm
-            
-        evaluate(self.eval_config, mol_lms)
-        
 
-def evaluate(
-    config: EvalConfig,
-    mol_lms: Dict[str, MolLM]
-):
+        evaluate(self.eval_config, mol_lms)
+
+
+def evaluate(config: EvalConfig, mol_lms: Dict[str, MolLM]):
     if config.device == Device.CPU.value:
         device = "cpu"
     elif config.device == Device.GPU.value:
@@ -71,16 +65,14 @@ def evaluate(
     else:
         raise ValueError(f"Invalid device: {config.device}")
 
-    dataloader = get_dataloader(
-        data_config=config.data_config,
-    )
+    dataloader = get_dataloader(data_config=config.data_config, )
     total_steps = len(dataloader)
     predictions_per_model = defaultdict(list)
     confidences_per_model = defaultdict(list)
     targets = []
-    
+
     cache_paths = (config.predict_config.cache_paths
-    if config.predict_config.cache_paths else {})
+                   if config.predict_config.cache_paths else {})
     for rep_name, cache_path in cache_paths.items():
         if rep_name not in mol_lms:
             continue
@@ -90,13 +82,12 @@ def evaluate(
         confidences_per_model[rep_name] = confidences
 
     assert predictions_per_model.keys() == confidences_per_model.keys(), (
-        "Predictions and confidences should have the same keys"
-    )
-    
+        "Predictions and confidences should have the same keys")
+
     cached_keys = set(predictions_per_model.keys())
     non_cached_mol_lms = {
-        rep_name: mol_lm for rep_name, mol_lm 
-        in mol_lms.items() if rep_name not in cached_keys
+        rep_name: mol_lm
+        for rep_name, mol_lm in mol_lms.items() if rep_name not in cached_keys
     }
     for batch in tqdm(dataloader, total=total_steps):
         target, description = batch
@@ -111,26 +102,27 @@ def evaluate(
             )
             predictions_per_model[rep_name].extend(predictions)
             confidences_per_model[rep_name].extend(confidences)
-   
+
     for rep_name, mol_lm in non_cached_mol_lms.items():
         _cache_predictions(
             key=rep_name,
             predictions=predictions_per_model[rep_name],
             confidences=confidences_per_model[rep_name],
         )
-    
+
     decoded_ensembled_predictions = enemble_and_decode_predictions(
         mol_lms=mol_lms,
         predictions_per_model=predictions_per_model,
         confidences_per_model=confidences_per_model,
     )
-    
+
     metrics = get_text2mol_metrics(
         predictions=decoded_ensembled_predictions,
         references=targets,
     )
-    
+
     logger.info("Metrics: %s", metrics)
+
 
 def predict(
     mol_lm: MolLM,
@@ -152,7 +144,7 @@ def predict(
             return_tensors="pt",
             return_attention_mask=True,
         ).to(device)
-        
+
         outputs = mol_lm.model.generate(
             input_ids=inputs["input_ids"],
             generation_config=mol_lm.model.generation_config,
@@ -163,18 +155,19 @@ def predict(
             return_dict_in_generate=True,
         )
 
-        sequences = outputs.sequences.reshape(batch_size, config.num_return_sequences, -1)
-        best_sequences = sequences[:, 0, :] # Sequences are sorted by score
+        sequences = outputs.sequences.reshape(batch_size,
+                                              config.num_return_sequences, -1)
+        best_sequences = sequences[:, 0, :]  # Sequences are sorted by score
         decoded_sequences = mol_lm.tokenizer.batch_decode(
             best_sequences, skip_special_tokens=True)
-        
+
         #TODO: calculate confidence
         confidences = get_confidence(
             mol_lm=mol_lm,
             device=device,
             input_ids=inputs["input_ids"],
             attention_mask=inputs["attention_mask"],
-            outputs=outputs, 
+            outputs=outputs,
             confidence=config.confidence_config.confidence,
             length_normalize=config.confidence_config.length_normalize,
             temperature=config.confidence_config.temperature,
@@ -182,8 +175,9 @@ def predict(
             prediction=decoded_sequences,
             target=target,
         )
-        
+
     return decoded_sequences, confidences
+
 
 # TODO(hyeontae): Refactor this function
 def get_confidence(
@@ -198,7 +192,7 @@ def get_confidence(
     prediction: List[str] = None,
     target: List[str] = None,
 ) -> List[float]:
-    
+
     if confidence == Confidence.NEG_PERPLEXITY.value:
         perplexity = get_perplexity(
             model=mol_lm.model,
@@ -249,7 +243,7 @@ def get_confidence(
             batch_size=input_ids.shape[0],
             outputs=outputs,
         )
-        
+
         return [-ent for ent in entropy.tolist()]
     elif confidence == Confidence.IMPORTANCE_WEIGHTED_ENTROPY.value:
         entropy = get_importance_weighted_entropy(
@@ -262,41 +256,42 @@ def get_confidence(
             temperature=temperature,
         )
         return [-ent for ent in entropy.tolist()]
-    
+
     elif confidence == Confidence.ORACLE_RDK.value:
         assert target is not None, "Target should be provided for oracle confidence"
         assert prediction is not None, "Prediction should be provided for oracle confidence"
-        
+
         rdk = get_rdk(
             representation=mol_lm.representation,
             prediction=prediction,
             target=target,
         )
-        return rdk.tolist() 
+        return rdk.tolist()
     elif confidence == Confidence.ORACLE_EXACT.value:
         assert target is not None, "Target should be provided for oracle confidence"
         assert prediction is not None, "Prediction should be provided for oracle confidence"
-        
+
         exact = get_exact(
             representation=mol_lm.representation,
             prediction=prediction,
             target=target,
         )
-        return exact.tolist()    
+        return exact.tolist()
     else:
         raise ValueError(f"Invalid confidence: {confidence}")
+
 
 def enemble_and_decode_predictions(
     mol_lms: Dict[str, MolLM],
     predictions_per_model: Dict[str, List[str]],
     confidences_per_model: Dict[str, List[float]],
 ) -> List[str]:
-    
-    logger.info("Ensembling predictions for model representations: %s", list(mol_lms.keys()))
+
+    logger.info("Ensembling predictions for model representations: %s",
+                list(mol_lms.keys()))
     assert len(predictions_per_model) == len(confidences_per_model), (
-        "Predictions and confidences should have the same keys"
-    )
-    
+        "Predictions and confidences should have the same keys")
+
     num_predictions = len(predictions_per_model[list(mol_lms.keys())[0]])
     assert all(
         len(predictions) == num_predictions
@@ -306,7 +301,7 @@ def enemble_and_decode_predictions(
         len(confidences) == num_predictions
         for confidences in confidences_per_model.values()
     ), "Confidences should have the same length"
-    
+
     ensembled_predictions = []
     model_win_counts = defaultdict(int)
     for i in range(num_predictions):
@@ -315,26 +310,27 @@ def enemble_and_decode_predictions(
             model_confidences[rep_name] = confidences[i]
         confident_rep = max(model_confidences, key=model_confidences.get)
         decoded_prediction = mol_lms[confident_rep].representation.decode(
-            predictions_per_model[confident_rep][i]
-        )
+            predictions_per_model[confident_rep][i])
         model_win_counts[confident_rep] += 1
         ensembled_predictions.append(decoded_prediction)
-    
+
     logger.info("Model win counts: %s", model_win_counts)
     return ensembled_predictions
-                  
-def _validate_eval_config(eval_config: EvalConfig, model_configs: List[ModelConfig]):
+
+
+def _validate_eval_config(eval_config: EvalConfig,
+                          model_configs: List[ModelConfig]):
     assert eval_config.predict_config.num_beams >= eval_config.predict_config.num_return_sequences, (
-        "num_beams should be greater than num_return_sequences"
-    )
+        "num_beams should be greater than num_return_sequences")
     assert not (eval_config.ensemble ^ (len(model_configs) > 1)), (
-        "Multiple models should be used for ensemble evaluation"
-    )
-    
-    rep_types = [model_config.representation_type for model_config in model_configs]
+        "Multiple models should be used for ensemble evaluation")
+
+    rep_types = [
+        model_config.representation_type for model_config in model_configs
+    ]
     assert len(set(rep_types)) == len(rep_types), (
-        "Models should have different representation types"
-    )
+        "Models should have different representation types")
+
 
 def _cache_predictions(
     key: str,
@@ -350,20 +346,18 @@ def _cache_predictions(
         confidences (List[float]): List of confidence scores corresponding to each prediction.
     """
     assert len(predictions) == len(confidences), (
-        "Predictions and confidences should have the same length."
-    )
+        "Predictions and confidences should have the same length.")
 
     file_path = f"{key}_predictions.txt"
     with open(file_path, "w") as f:
         for prediction, confidence in zip(predictions, confidences):
             f.write(f"{prediction}\t({confidence:.6f})\n")
-    
+
     abs_path = os.path.abspath(file_path)
     logger.info("Predictions for '%s' are cached to '%s'.", key, abs_path)
-    
-def _load_cache(
-    cache_path: str,
-) -> Tuple[List[str], List[float]]:
+
+
+def _load_cache(cache_path: str, ) -> Tuple[List[str], List[float]]:
     """
     Loads cached predictions and confidences from a file.
 
@@ -379,7 +373,6 @@ def _load_cache(
         for line in f:
             prediction, confidence = line.strip().split("\t")
             predictions.append(prediction)
-            confidences.append(float(confidence[1:-1])) # Remove parentheses
-    
+            confidences.append(float(confidence[1:-1]))  # Remove parentheses
+
     return predictions, confidences
-    
