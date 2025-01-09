1e3ef462b0b619fb1e8bae8576eaecc7314c98e7
Songhyeontae <hyeontae@kaist.ac.kr>
2025-01-09 16:54:58 +0000

    frag camt5 v3

--- scripts/merge_frag_dicts.py
+++ scripts/merge_frag_dicts.py
@@ -1,21 +1,18 @@
 import os
 import sys
+
 sys.path.append(os.getcwd())
 
 if __name__ == "__main__":
     frag_set = set()
-    with open(
-            f"asset/mol_vocabs/frag_stereo_v2.txt") as f:
+    with open(f"asset/mol_vocabs/frag_stereo_v2.txt") as f:
         for line in f:
             frag_set.add(line.strip())
-    with open(
-            f"asset/mol_vocabs/frag_pubchem_v2.txt") as f:
+    with open(f"asset/mol_vocabs/frag_pubchem_v3.txt") as f:
         for line in f:
             frag_set.add(line.strip())
-            
-    with open(
-        f"asset/mol_vocabs/frag_camt5_v2.txt", "w"
-    ) as f:
+
+    with open(f"asset/mol_vocabs/frag_camt5_v3.txt", "w") as f:
         frags = sorted(list(frag_set))
         for frag in frags:
             f.write(frag)
